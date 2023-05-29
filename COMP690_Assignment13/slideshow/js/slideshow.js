377121717e2174ef8b1706ac8c659a8f37f78281
Wenlin <78585348+wenlinkaren@users.noreply.github.com>
2023-05-29 16:05:43 -0700

    Add files via upload

---
+++ COMP690_Assignment13/slideshow/js/slideshow.js
@@ -0,0 +1,106 @@
+const $ = function (id) {
+    return document.getElementById(id);
+};
+
+const createSlideshow = function () {
+    let timer;
+    let speed = 2000
+    let play = true;
+    
+    let nodes = { image: null, caption: null };
+    let img = { cache: [], counter: 0 };
+    
+    const stopSlideShow = function () {
+        clearInterval(timer);
+    };
+    const displayNextImage = function () {
+        if (img.counter === img.cache.length - 1) {
+            img.counter = 0;
+        } else {
+            img.counter += 1;
+        }
+        let image = img.cache[img.counter];
+        nodes.image.src = image.src;
+        nodes.caption.innerHTML = image.title;
+    };
+    const setPlayText = function (btn) {
+        if (play) {
+            btn.value = 'Resume';
+        } else {
+            btn.value = 'Pause';
+        }
+    };
+    
+    return {
+        getSpeed: function () {
+            return speed;
+        },
+        setSpeed: function (promptSpeed) {
+            speed = promptSpeed;
+            return this;
+        },
+        loadImages: function (slides) {
+            let image;
+            for (let i = 0; i < slides.length; i += 1) {
+                image = new Image();
+                image.src = slides[i].href;
+                image.title = slides[i].title;
+                img.cache.push(image);
+            }
+            return this;
+        },
+        startSlideShow: function () {
+            if (arguments.length === 2) {
+                nodes.image = arguments[0];
+                nodes.caption = arguments[1];
+            }
+            if (timer) {
+                stopSlideShow();
+            }
+            timer = setInterval(displayNextImage, speed);
+            return this;
+        },
+        createToggleHandler: function () {
+            let me = this;
+            // CLOSURE TO BE USED AS THE CLICK EVENT HANDLER
+            return function () {
+                // 'THIS' IS THE CLICKED BUTTON
+                // 'ME' IS THE OBJECT LITERAL
+                if (play) {
+                    stopSlideShow();
+                } else {
+                    me.startSlideShow();
+                }
+                setPlayText(this);
+                // TOGGLE PLAY 'FLAG'
+                play = !play;
+            };
+        }
+    };
+};
+
+const slideshow = createSlideshow();
+
+window.addEventListener('load', function () {
+    const slides = [
+        {href: 'images/backpack.jpg', title: 'He backpacks in the Sierras often'},
+        {href: 'images/boat.jpg', title: 'He loves his boat'},
+        {href: 'images/camaro.jpg', title: 'He loves his Camaro more'},
+        {href: 'images/punk.jpg', title: 'He used to be in a punk band and toured with No Doubt and Sublime'},
+        {href: 'images/race.jpg', title: 'He\'s active and loves obstacle coarse racing'}
+    ];
+	
+    slideshow.loadImages(slides).startSlideShow($('image'), $('caption'));
+    
+    $('play_pause').onclick = slideshow.createToggleHandler();
+    
+    $('speed').addEventListener('click', () => {
+        let promptSpeed = parseInt(prompt('The current speed is ' + slideshow.getSpeed() + ' milliseconds. Please enter new speed'), 10);
+
+        if (promptSpeed < 0 || isNaN(promptSpeed)) {
+            prompt('Please enter a valid value for speed');
+            return false;
+        }
+        slideshow.setSpeed(promptSpeed).startSlideShow();
+    });
+});
